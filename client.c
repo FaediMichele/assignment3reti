eeb4a1594b5803f1b0421dc0174726f98aaa2d3a
michele.faedi <michele.faedi@studio.unibo.it>
2019-12-13 11:45:11 +0100

    Ended the program. Should work well

--- client.c
+++ client.c
@@ -14,47 +14,50 @@
 
 #define PROTOCOL_PHASE_HELLO 'h'
 #define PROTOCOL_PHASE_MEASURE 'm'
-#define PROTOCOL_PHASE_BYE 'b'
 #define MEASURE_RTT "rtt"
 #define MEASURE_THROUGHTPUT "thput"
+
+#define HELLO_ACCEPTED "200 OK - Ready"
+#define HELLO_REFUSED "404 ERROR – Invalid Hello message"
+#define HELLO_MESSAGE_SIZE 27
+#define MEASURE_MESSAGE_SIZE 8 // without the payload
 
 typedef struct{
     char measure_type[6];
     struct sockaddr_in server_addr;
     int n_probes;
-    long msg_size;
+    int measure_size_index;
     int server_delay;
 } param_t;
 
 /* <protocol_phase> <sp> <measure_type> <sp> <n_probes> <sp> <msg_size> <sp> <server_delay>\n */
-const char hello_message_format[] = "%c %s %d %ld %d";
+const char hello_message_format[] = "%c %s %05d %05d %05d\n";
 
 /* <protocol_phase> <sp> <probe_seq_num> <sp> <payload>\n */
-const char measurement_message_format[] = "%c %d %s";
+const char measurement_message_format[] = "%c %05d %s\n";
+
+const char bye_message[] = "b\n";
+const int measure_size_rtt[] = {1, 100, 200, 400, 800, 1000};
+const int measure_size_thput[] = {1000, 2000, 4000, 16000, 32000};
 
 
 // Fill the param_t based on the input parameters. return -1 in case of error, else return the number of paramether used
 int fillParam(int argc, char *argv[], param_t *param);
 
 // Analyze the data received from the server and verify if is ok.
-int analyzeData(char *received, int probe);
+int analyzeData(char *received);
 
 // Send the message to start the test to the sfd
-int sendHello(int sfd, const param_t *param, char *sendData, char *receivedData);
+int sendHello(int sfd, const param_t *param, char *sendData, char *receivedData, const int *measure_size_touse);
 
-void startChrono(struct timeval *time){
-    if(gettimeofday(time, NULL) < 0){
-        perror("Time");
-    }
-}
+int sendByeMessage(int sfd, param_t *param);
 
-long long stopChrono(struct timeval time){
-    struct timeval endTime;
-    if(gettimeofday(&endTime, NULL) < 0){
-        perror("Time");
-    }
-    return (endTime.tv_usec - time.tv_usec) / 100;
-}
+void initializePayload(char *str, ssize_t lenght);
+
+void startChrono(struct timeval *time);
+
+// Return the milliseocond passed.
+int stopChrono(struct timeval time);
 
 int main(int argc, char *argv[]){
     int sfd; // Server socket filed descriptor
@@ -63,76 +66,107 @@ int main(int argc, char *argv[]){
     socklen_t serv_size;
     struct timeval chrono;
     long int time_all;
-    
     param_t param;
-    
-    sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    
-    if (sfd < 0){
-        perror("socket"); // Print error message
-        exit(EXIT_FAILURE);
-    }
-    if( fillParam(argc, argv, &param) < 0){
-        printf("Errore nei parametri\n");
-        printf("%s <server IP (dotted notation)> <server port> <measure type(rtt / thput)> [-d delay][-p n_probes][-m messagesize]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    char receivedData [param.msg_size]; // Data to be received
-    char sendData [param.msg_size]; // Data to be sent
-    int receivedProbe; // Probe received as ACK
-    
-    serv_size = sizeof(param.server_addr);
-
-    if (connect(sfd, (struct sockaddr *) &param.server_addr, serv_size) < 0) {
-        perror("connect"); // Print error message
-        exit(EXIT_FAILURE);
-    }
-    
-    sendHello(sfd, &param, sendData, receivedData);
-
-
-    // MEASURE
     int probe = 1;
-
-    while(probe < param.n_probes){
-
-        snprintf(sendData, param.msg_size, measurement_message_format, PROTOCOL_PHASE_MEASURE,
-            probe, "PAYLOAD START HERE:\n");
-        startChrono(&chrono);
-        byteSent = send(sfd, sendData, param.msg_size, 0);
-        if( byteSent <= 0) {
-            perror("Error sending message");
+    int receivedProbe; // Probe received as ACK
+    int rtt;
+    int loss = 0;
+    int measure_index = 0;
+    
+    if( fillParam(argc, argv, &param) < 0){
+        printf("%s: unrecognized paramether.\nUsage = ./client <server IP (dotted notation)> <server port> <measure type(rtt / thput)> [-d delay][-p n_probes]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    const int *measure_size_touse = strcmp(param.measure_type, MEASURE_RTT) ? 
+            measure_size_thput:
+            measure_size_rtt;
+    long n_measure = measure_size_touse == measure_size_rtt ? sizeof(measure_size_rtt) : sizeof(measure_size_thput);
+    n_measure/=sizeof(int);
+    fflush(stdout);
+    // for every payload size for the selected measure.
+    for(measure_index = 0; measure_index < n_measure; measure_index++){
+        long msg_size = measure_size_touse[measure_index] + MEASURE_MESSAGE_SIZE;
+        char receivedData[msg_size]; // Data to be received
+        char sendData[msg_size]; // Data to be sent
+        initializePayload(sendData, msg_size);
+        serv_size = sizeof(param.server_addr);
+        time_all = 0;
+        
+        if ((sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
+            perror("socket"); // Print error message
             exit(EXIT_FAILURE);
         }
-        byteRecv = recv(sfd, receivedData, param.msg_size, 0);
-        if( byteRecv <= 0){
-            perror("Error receiving message");
+        if (connect(sfd, (struct sockaddr *) &param.server_addr, serv_size) < 0) {
+            perror("connect"); // Print error message
             exit(EXIT_FAILURE);
         }
-        time_all += stopChrono(chrono);
-        if((receivedProbe = analyzeData(receivedData, probe)) < 0){
-            printf("Error due a format problem\n");
-        } else if(receivedProbe != probe){
-            printf("Received wrong probe. Resending the last correct probe");
+        
+        sendHello(sfd, &param, sendData, receivedData, measure_size_touse);
+
+
+        // MEASURE
+        probe = 0;
+        
+        while(probe < param.n_probes){
+
+            snprintf(sendData, msg_size, measurement_message_format, PROTOCOL_PHASE_MEASURE,
+                probe, "\nPAYLOAD START HERE:\n");
+            sendData[msg_size - 1] = '\n';
+            
+            startChrono(&chrono);
+            byteSent = send(sfd, sendData, msg_size, 0);
+            if( byteSent <= 0) {
+                perror("Error sending message");
+                exit(EXIT_FAILURE);
+            }
+            byteRecv = recv(sfd, receivedData, msg_size, 0);
+            if( byteRecv < 0){
+                perror("Error receiving message");
+                exit(EXIT_FAILURE);
+            } else if( byteRecv == 0){
+                printf("Connection closed by the server\n");
+                exit(EXIT_FAILURE);
+            }
+
+            rtt = stopChrono(chrono);
+            time_all += rtt;
+            //printf("%ld byte from %s probe_seq=%d rtt=%d ms\n", byteRecv, argv[1], probe, rtt);
+            //fflush(stdout);
+            if((receivedProbe = analyzeData(receivedData)) < 0){
+                printf("Error due a format problem\n");
+            } else if(receivedProbe != probe){
+                printf("Received wrong probe. Resending the last correct probe(%d)\n", receivedProbe);
+                probe--;
+                loss++;
+            }
+            probe++;
         }
-        probe++;
+        sendByeMessage(sfd, &param);
+        printf("--- %s measure statistics ---\n", argv[1]);
+        printf("%d message trasmitted, %d%% message loss, %ld message size, rtt = %lims, time = %lims\n",
+            probe, loss * 100 / probe, msg_size - MEASURE_MESSAGE_SIZE,
+            1 + time_all / probe, time_all);
+        if( !strcmp(param.measure_type, MEASURE_THROUGHTPUT)){
+            // the 1 + is in case of average time 0 and the /1000 is for kbps.
+            printf("--- average throughtput = %lf kbps\n",
+                ((double) msg_size) / (1 + time_all / (double) probe) / 1000);
+        }
+        fflush(stdout);
+        close(sfd);
+        param.measure_size_index++;
     }
-    printf("Average RTT = %ld\n", time_all / probe);
-    if( !strcmp(param.measure_type, MEASURE_THROUGHTPUT)){
-        printf("THROUGHTPUT = %lf\n", param.msg_size / (time_all / (double) probe));
-    }
+    
     return 0;
 }
-int analyzeData(char *received, int probe){
+int analyzeData(char *received){
     char phase;
     int recvProbe;
     char *payload = NULL;
     sscanf(received, measurement_message_format, &phase, &recvProbe, payload);
     if(phase != PROTOCOL_PHASE_MEASURE){
-        printf("%c %d", phase, recvProbe);
         return -1;
     }
-    return probe;
+    return recvProbe;
 }
 
 int fillParam(int argc, char *argv[], param_t *param){
@@ -140,8 +174,8 @@ int fillParam(int argc, char *argv[], param_t *param){
     if (argc  < 3){
         return -1;
     }
-    param->n_probes = 1024;
-    param->msg_size = 256;
+    param->n_probes = 20;
+    param->measure_size_index = 0;
     param->server_delay = 0;
     param->server_addr.sin_family = AF_INET;
     param->server_addr.sin_port = htons(atoi(argv[2]));
@@ -157,11 +191,6 @@ int fillParam(int argc, char *argv[], param_t *param){
             if(param->n_probes <= 0){
                 return -1;
             }
-        } else if(!strcmp(argv[i], "-m") || !strcmp(argv[i], "--messagesize")){
-            param->msg_size = atoi(argv[i + 1]);
-            if(param->msg_size <= 0){
-                return -1;
-            }
         } else if(!strcmp(argv[i], "-d") || !strcmp(argv[i], "--delay")){
             param->server_delay = atoi(argv[i + 1]);
         } else{
@@ -171,19 +200,49 @@ int fillParam(int argc, char *argv[], param_t *param){
     return argc - 3;
 }
 
-int sendHello(int sfd, const param_t *param, char *sendData, char *receivedData){
-    snprintf( sendData, param->msg_size, hello_message_format, PROTOCOL_PHASE_HELLO,
-        param->measure_type, param->n_probes, param->msg_size, param->server_delay);
-
-    // HELLO
+int sendHello(int sfd, const param_t *param, char *sendData, char *receivedData, const int *measure_size_touse){
+    snprintf( sendData, HELLO_MESSAGE_SIZE,
+        hello_message_format, PROTOCOL_PHASE_HELLO,
+        param->measure_type, param->n_probes,
+        measure_size_touse[param->measure_size_index], param->server_delay);
+    
     send(sfd, sendData, strlen(sendData), 0);
-    recv(sfd, receivedData, param->msg_size, 0);
-    if( strcmp(receivedData, "200 OK - Ready")){
-        printf("Error in the hello phase\n");
-        if(strcmp(receivedData, "404 ERROR – Invalid Hello message")){
-            printf("FATAL ERROR. Received message: \n%s", receivedData);
+    recv(sfd, receivedData, HELLO_MESSAGE_SIZE, 0);
+    if( strcmp(receivedData, HELLO_ACCEPTED)){
+        if(!strcmp(receivedData, HELLO_REFUSED)){
+            printf("Hello refused. Received message: \n%s", receivedData);
+        } else {
+            printf("Error sending the hello message\n%s\n%s\n", sendData, receivedData);
         }
+        fflush(stdout);
         return -1;
     }
     return 1;
+}
+
+int sendByeMessage(int sfd, param_t *param){
+    return send(sfd, bye_message, sizeof(bye_message), 0) != sizeof(bye_message);
+}
+
+
+void initializePayload(char *str, ssize_t lenght){
+    long i;
+    for(i = 0; i < lenght; i++){
+        str[i] = '-';
+    }
+}
+
+void startChrono(struct timeval *time){
+    if(gettimeofday(time, NULL) < 0){
+        perror("Time");
+    }
+}
+
+int stopChrono(struct timeval time){
+    struct timeval endTime, res;
+    if(gettimeofday(&endTime, NULL) < 0){
+        perror("Time");
+    }
+    timersub(&endTime, &time, &res);
+    return res.tv_sec * 1000 + res.tv_usec / 1000;
 }
--- server.c
+++ server.c
@@ -18,28 +18,35 @@
 #define MEASURE_RTT "rtt"
 #define MEASURE_THROUGHTPUT "thput"
 #define MEASURE_TYPE_SIZE 6
-#define HELLO_MESSAGE_SIZE 128
+#define MEASURE_MESSAGE_SIZE 8 // without the payload
+#define HELLO_MESSAGE_SIZE 27
 #define BACK_LOG 10 // Maximum queued requests
 
 typedef struct{
-    unsigned long msg_size;          // message size
+    unsigned long msg_size; // message size
     int expected_probe;     // numbre of expected probe
     int delay;              // time to wait to send the response
 } param_t;
 
 /* <protocol_phase> <sp> <measure_type> <sp> <n_probes> <sp> <msg_size> <sp> <server_delay>\n */
-const char hello_message_format[] = "%c %s %d %lu %d \n";
+const char hello_message_format[] = "%c %s %d %d %d\n";
 
 /* <protocol_phase> <sp> <probe_seq_num> <sp> <payload>\n */
 const char measurement_message_format[] = "%c %d %s\n";
+
+const char bye_message_response[] = "200 OK - Closing";
+
+/* <protocol_phase>\n */
+const char bye_message_format[] = "%c\n";
 
 const char measure_accepted_m[] = "200 OK - Ready";
 
 void open(char **argv, struct sockaddr_in *server_addr, int *sfd, socklen_t *cli_size);
 
-int parseMeasure(char *message, int actual_probes);
+int isByeMessage(char *message);
 
 int parseHello(char *message, param_t *param);
+int manageHello(int sfd, param_t *param);
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr; // struct containing server address information
@@ -50,73 +57,84 @@ int main(int argc, char *argv[]) {
     ssize_t byteSent; // Number of bytes to be sent
     socklen_t cli_size;
     param_t param;
+    char client_add_s[INET_ADDRSTRLEN]; // String for the client address.
+    int ok; // used like boolean
+
     if(argc != 2){
         printf("Errore nei parametri\n");
         printf("%s <server port (0 - 65535)> \n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    char helloMessage[HELLO_MESSAGE_SIZE];
+    
 
     open(argv, &server_addr, &sfd, &cli_size);
     
     for(;;){
+        printf("Waiting new request...\n");
+        fflush(stdout);
         // Wait for incoming requests
         if (( newsfd = accept(sfd, (struct sockaddr *) &client_addr, &cli_size)) < 0){
             perror("accept"); // Print error message
             exit(EXIT_FAILURE);
         }
-        byteRecv = recv(newsfd, helloMessage, HELLO_MESSAGE_SIZE, 0);
-        if (byteRecv <= 0){
-            perror("recv on hello");
-            exit(EXIT_FAILURE);
+        
+        inet_ntop( AF_INET, &client_addr, client_add_s, INET_ADDRSTRLEN );
+        printf("Connection accepted: address: %s:%d\n", client_add_s, client_addr.sin_port);
+        
+        if(!manageHello(newsfd, &param)){
+            perror("Error with the hello");
+            close(newsfd);
+            continue;
         }
-        if(parseHello(helloMessage, &param) < 0){
-            perror("hello message error");
-            break;
-        }
-        byteSent = send(newsfd, measure_accepted_m, sizeof(measure_accepted_m), 0);
-        if(byteSent <= 0){
-            perror("recv on hello");
-            exit(EXIT_FAILURE);
-        }
-
-        char receivedData1[param.msg_size]; // also the send data
-        char receivedData2[param.msg_size]; // also the send data
-        // There is two because if the server receive a wrong probe number must be send the previous correct ack.
-        int probe = 1;
-        char *buff;
-        while(probe != param.expected_probe){
-            buff = probe % 2 == 1 ? receivedData1 : receivedData2;
-            byteRecv = recv(newsfd, receivedData1, param.msg_size, 0);
-            if (byteRecv <= 0){
-                printf("recv on measure(size = %ld - actual = %ld): %s", param.msg_size, byteRecv, strerror(errno));
+        char receivedData[param.msg_size]; // also the send data
+        ok = 0;
+        while(1){
+            byteRecv = recv(newsfd, receivedData, param.msg_size, 0);
+            if (byteRecv < 0){
                 perror("recv on measure");
-                exit(EXIT_FAILURE);
+                ok = 1;
+            } else if (byteRecv == 0){
+                perror("Connection closed by client");
+                ok = 1;
             }
-            usleep(param.delay);
-            if(parseMeasure(buff, probe) < 0){
-                probe--;
+            if(isByeMessage(receivedData)){
+                break;
             }
-            buff = probe % 2 == 1 ? receivedData1 : receivedData2;
-            byteSent = send(newsfd, buff, param.msg_size, 0);
+            fwrite(receivedData, sizeof(char), param.msg_size, stdout);
+            usleep(param.delay * 1000);
+            fflush(stdout);
+            byteSent = send(newsfd, receivedData, param.msg_size, 0);
             if(byteSent != byteRecv){
-                printf("sens on measure(size = %ld - actual = %ld): %s", param.msg_size, byteSent, strerror(errno));
-                exit(EXIT_FAILURE);
+                ok = 1;
             }
-            probe++;
-            
+            if(ok){
+                close(newsfd);
+                newsfd = -1;
+                break;
+            }
         }
+        if(newsfd < 0){
+            continue;
+        }
+        send(newsfd, bye_message_response, sizeof(bye_message_response), 0);
+        close(newsfd);
+        printf("Connection closed from: %s:%hu\n", client_add_s, client_addr.sin_port);
     } // End of for(;;)
     close(sfd);
     return 0;
 }
 
+/*
+    Initialize the socket with the port.
+    The program is closed in case of error.
+*/
 void open(char **argv, struct sockaddr_in *server_addr, int *sfd, socklen_t *cli_size){
     int port = atoi(argv[1]);
+    // Parse the port.
     if(port <= 0 || port >= __UINT16_MAX__){
         printf("Errore nei parametri\n");
         printf("%s <server port (0 - 65535)> \n", argv[0]);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     
     *sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -124,47 +142,75 @@ void open(char **argv, struct sockaddr_in *server_addr, int *sfd, socklen_t *cli
         perror("socket"); // Print error message
         exit(EXIT_FAILURE);
     }
-    printf("PORT = %u\tSFD = %d\n", (uint16_t) port, *sfd);
-    fflush(stdout);
-    // Initialize server address information
+    
+    // Initialize server address information.
     server_addr->sin_family = AF_INET;
     server_addr->sin_port = htons((uint16_t) port);
     server_addr->sin_addr.s_addr = INADDR_ANY;
+    *cli_size = sizeof(server_addr);
+
+    // Bind the address to the socket file descriptor.
     if (bind(*sfd, (struct sockaddr *) server_addr, sizeof(*server_addr)) < 0){
-        printf("%s", strerror(errno));
         perror("bind"); // Print error message
         exit(EXIT_FAILURE);
     }
-    *cli_size = sizeof(server_addr);
-    // Listen for incoming requests
+    // Listen the socket file descriptor.
     if (listen(*sfd, BACK_LOG) < 0){
         perror("listen"); // Print error message
         exit(EXIT_FAILURE);
     }
 }
 
-int parseHello(char *message, param_t *param){
+/*  Verify if the message for the hello phase is correct.
+    Return 0 in case of error
+*/
+int parseHello(char *message, param_t *param) {
     char phase;
     char measure[MEASURE_TYPE_SIZE];
     int n_probes;
-    unsigned long msg_size;
+    int msg_size;
     int delay;
     int res = sscanf(message, hello_message_format, &phase, measure, &n_probes, &msg_size, &delay);
     if(res < 0 || phase != 'h'){
-        return -1;
+        return 0;
     }
     param->delay = delay;
-    param->msg_size = msg_size;
+    fflush(stdout);
+    param->msg_size = msg_size + MEASURE_MESSAGE_SIZE;
     return 1;
 }
 
-int parseMeasure(char *message, int actual_probes){
+/*  Verify if the message is for the end phase
+    Return 0 in case of error
+*/
+int isByeMessage(char *message){
     char phase;
-    int probe;
-    char *payload = NULL;
-    int res = sscanf(message, measurement_message_format, &phase, &probe, payload);
-    if(res < 0 || phase != 'm' || probe != actual_probes){
-        return -1;
+    int res = sscanf(message, bye_message_format, &phase);
+    if(res < 0 || phase != 'b'){
+        return 0;
     }
+    return 1;
+}
+
+int manageHello(int sfd, param_t *param){
+    ssize_t byteRecv, byteSent;
+    char helloMessage[HELLO_MESSAGE_SIZE];
+    byteRecv = recv(sfd, helloMessage, HELLO_MESSAGE_SIZE, 0);
+    if (byteRecv <= 0){
+        printf("recv on hello\n");
+        return 0;
+    }
+    
+    if(parseHello(helloMessage, param) < 0){
+        printf("hello message error\n");
+        return 0;
+    }
+    
+    byteSent = send(sfd, measure_accepted_m, sizeof(measure_accepted_m), 0);
+    if(byteSent <= 0){
+        printf("recv on hello\n");
+        return 0;
+    }
+    
     return 1;
 }
